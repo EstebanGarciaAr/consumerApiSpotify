61510e7a0f38050a61029d269c9368bb74dea53e
jasz1802 <jennyferariasz18@gmail.com>
2025-05-03 18:57:57 -0500

    Create LoginPage component and add Bootstrap styles

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,5 +1,25 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
+import { LoginForm } from "../components/login/LoginForm";
+import "../../auth/components/login/styles/login.css";
+
 export const LoginPage = () => {
+  const navigate = useNavigate();
+  const { login } = useContext(UserContext);
+
+  const onLoginUser = ({ email, password }) => {
+    login({ email, password });
+    navigate("/", { replace: true });
+  };
+
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div className="login-page">
+      <div className="login-container">
+        <LoginForm onLogin={onLoginUser} />
+      </div>
+    </div>
+  );
+};
+
+
