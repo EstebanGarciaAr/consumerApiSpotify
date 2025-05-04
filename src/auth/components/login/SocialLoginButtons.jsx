643879ecb4ba764a1a7bc31288845abd02ff1ba6
jasz1802 <jennyferariasz18@gmail.com>
2025-05-03 19:02:18 -0500

    Create SocialLoginButtons component

--- src/auth/components/login/SocialLoginButtons.jsx
+++ src/auth/components/login/SocialLoginButtons.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import "./styles/login.css"
 
-export const SocialLoginButtons = () => {
-  return (
-    <div>SocialLoginButtons</div>
-  )
-}
+
+export const SocialLoginButtons = () => (
+  <div className="social-login-container text-center mb-3">
+    <p className="texto-continuar">Continuar con:</p>
+    <div className="d-flex justify-content-center gap-3">
+      <i className="bi bi-facebook social-icon"></i>
+      <i className="bi bi-apple social-icon"></i>
+      <i className="bi bi-google social-icon"></i>
+    </div>
+  </div>
+);
+  
