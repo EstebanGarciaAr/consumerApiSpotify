d70fffb1162cd04ee12ac701babc71695e2b3fc0
Lfdavidgiraldo <luiidaviid15@gmail.com>
2025-05-04 23:53:51 -0500

    spotify icon added

--- src/auth/components/login/SocialLoginButtons.jsx
+++ src/auth/components/login/SocialLoginButtons.jsx
@@ -3,11 +3,13 @@ import "./styles/login.css"
 
 export const SocialLoginButtons = () => (
   <div className="social-login-container text-center mb-3">
-    <p className="texto-continuar">Continuar con:</p>
+    <p className="texto-continuar">Continue witht:</p>
     <div className="d-flex justify-content-center gap-3">
       <i className="bi bi-facebook social-icon"></i>
       <i className="bi bi-apple social-icon"></i>
       <i className="bi bi-google social-icon"></i>
+      <i className="fab fa-spotify social-icon"></i>
+
     </div>
   </div>
 );
