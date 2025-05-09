1a80c620512a6e4196f39d0b4d43ff89e5e76852
jasz1802 <jennyferariasz18@gmail.com>
2025-05-09 02:25:41 -0500

    Update SocialLoginButtons component: modified line 18

--- src/auth/components/login/SocialLoginButtons.jsx
+++ src/auth/components/login/SocialLoginButtons.jsx
@@ -1,21 +1,25 @@
 import "./styles/login.css";
 
-export const SocialLoginButtons = ({ onGoogleLogin, onFacebookLogin }) => (
+export const SocialLoginButtons = ({ onGoogleLogin, onFacebookLogin, onSpotifyLogin }) => (
   <div className="social-login-container text-center mb-3">
     <p className="texto-continuar">Continue with:</p>
     <div className="d-flex justify-content-center gap-3">
       <i 
-          className="fab fa-facebook social-icon"
-          onClick={onFacebookLogin}
-          style={{cursor: "pointer"}}
+        className="fab fa-facebook social-icon"
+        onClick={onFacebookLogin}
+        style={{cursor: "pointer"}}
       ></i>
       <i className="fab fa-apple social-icon"></i>
       <i 
-          className="fab fa-google social-icon"
-          onClick={onGoogleLogin}
-          style={{cursor: "pointer"}}
+        className="fab fa-google social-icon"
+        onClick={onGoogleLogin}
+        style={{cursor: "pointer"}}
       ></i>
-      <i className="fab fa-spotify social-icon"></i>
+      <i 
+        className="fab fa-spotify social-icon"
+        onClick={onSpotifyLogin}
+        style={{cursor: "pointer"}}
+      ></i>
     </div>
   </div>
 );
