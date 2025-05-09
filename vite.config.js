5912d127c0b7b25eea7021c9a45c2eb9d191a89f
jasz1802 <jennyferariasz18@gmail.com>
2025-05-09 02:39:48 -0500

    Configure vite.config.js to work with ngrok by setting up server options

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['0cd7-38-156-230-137.ngrok-free.app'],
+  },
 })
