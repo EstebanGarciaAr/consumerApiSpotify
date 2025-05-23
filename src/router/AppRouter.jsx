841fc30ff56ba24b689e8af2fa22df0c045128a5
jasz1802 <jennyferariasz18@gmail.com>
2025-05-22 19:11:51 -0500

    remove CallBack route from AppRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,10 +1,10 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../auth/context/UserContext";
+
 import { HomePage } from '../pages/HomePage';
 import { LoginPage } from "../auth/pages/LoginPage";
 import { RegisterPage } from "../auth/pages/RegisterPage";
-import  ApiCallBack  from "../apiConsumer/ApiCallBack"; 
-import { useContext } from "react";
-import { UserContext } from "../auth/context/UserContext";
 
 export const AppRouter = () => {
   const { userState: { logged } } = useContext(UserContext);
@@ -12,10 +12,9 @@ export const AppRouter = () => {
   if (!logged) {
     return (
       <Routes>
-        <Route path="/Login" element={<LoginPage />} />
-        <Route path="/Register" element={<RegisterPage />} />
-        <Route path="/CallBack" element={<ApiCallBack />} /> 
-        <Route path="/*" element={<Navigate to="/Login" />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/*" element={<Navigate to="/login" />} />
       </Routes>
     );
   }
@@ -23,9 +22,8 @@ export const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/Login" element={<LoginPage />} />
-      <Route path="/Register" element={<RegisterPage />} />
-      <Route path="/CallBack" element={<ApiCallBack />} /> 
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
       <Route path="/*" element={<Navigate to="/" />} />
     </Routes>
   );
