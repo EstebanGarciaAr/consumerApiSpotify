f5dcf11e2f06fac37afdc30c514922375a807b51
Lfdavidgiraldo <luiidaviid15@gmail.com>
2025-05-26 19:04:45 -0500

    Modify classes to support Ngrok integration

--- src/auth/hooks/useAuthenticate.js
+++ src/auth/hooks/useAuthenticate.js
@@ -1,37 +1,37 @@
 import { authTypes } from "../types/authTypes";
-import { loginUser, authWithGoogle, authWithFacebook } from "../../firebase/provider"
+import {
+  loginUser,
+  authWithGoogle,
+  authWithFacebook,
+} from "../../firebase/provider";
 import { authenticationHelper } from "../helpers/authenticationHelper";
 
 export const useAuthenticate = (dispatch) => {
-    //login
+  const login = async ({ email, password }) => {
+    const authResponse = await loginUser(email, password);
+    return authenticationHelper(authResponse, dispatch);
+  };
 
-    const login = async ({email, password}) => {
-        const authResponse = await loginUser(email, password);
-        return authenticationHelper(authResponse, dispatch)
+  const loginGoogle = async () => {
+    const authResponse = await authWithGoogle();
+    return authenticationHelper(authResponse, dispatch);
+  };
+
+  const loginFacebook = async () => {
+    const authResponse = await authWithFacebook();
+    return authenticationHelper(authResponse, dispatch);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("refresh_token");
+    localStorage.removeItem("user");
+
+    const action = {
+      type: authTypes.logout,
     };
+    dispatch(action);
+  };
 
-    //login with google
-
-    const loginGoogle = async () => {
-        const authResponse = await authWithGoogle();
-        return authenticationHelper(authResponse, dispatch)
-    }
-
-    // login with facebook
-
-    const loginFacebook = async () => {
-        const authResponse = await authWithFacebook();
-        return authenticationHelper(authResponse, dispatch)
-    }
-
-    //logout
-
-    const logout = () => {
-        const action = {
-            type: authTypes.logout
-        }
-        dispatch(action);
-    };
-    
-    return {login, loginGoogle, loginFacebook, logout};
-}
+  return { login, loginGoogle, loginFacebook, logout };
+};
